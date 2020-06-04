2a14a93a9c4006440d2c12bedf5c5e6d0d2cca08
Rahul Ranjan <rranjan18bcs@iiitkottayam.ac.in>
2020-06-04 13:44:07 +0530

    Update db.ts

--- util/db.ts
+++ util/db.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 
-const MONGO_URL = `mongodb+srv://rranjan18:password00@deno-cluster-lc6m8.mongodb.net/shop`;
+const MONGO_URL = `ADD YOUR URL`;
 const client = new MongoClient();
 client.connectWithUri(MONGO_URL);
 
